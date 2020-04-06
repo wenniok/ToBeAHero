f7f8ae616f79ef46f077cdae2daa93fa5a34e868
wennio oliveira <wennio@motorola.com>
2020-04-05 23:05:09 -0400

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informação do back-end
+ * POST: Criar informação do back-end
+ * PUT: Alterar informação do back-end
+ * DELETE: Deletar informação do back-end
+ */
+
+/**
+ * Tipos de parâmetros
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
+
